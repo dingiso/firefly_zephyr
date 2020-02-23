44f397f28846fb253a15da8b4eaafbd4a8031ca9
Alexey Eremin <a.eremin.msu@gmail.com>
2020-02-23 15:54:05 +0100

    Implement color choice based on radio packets received

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,94 @@
+#include <array>
 #include <device.h>
 #include <drivers/gpio.h>
 #include <drivers/spi.h>
 #include <logging/log.h>
 #include <zephyr.h>
-
-#include <vector>
-
 #include "cc1101.h"
 #include "rgb_led.h"
 
 LOG_MODULE_REGISTER();
 
 struct MagicPathRadioPacket {
+  uint8_t id;
   uint8_t r, g, b;
+  uint8_t r_background, g_background, b_background;
+  bool configure_mode;
 } __attribute__((__packed__));
 
-struct UsbHostPacket {
-  uint16_t From;  // 2
-  uint16_t To;    // 2
-  uint8_t stuff[7];
-} __attribute__ ((__packed__));
+struct ColorAndTimestamp {
+  ColorAndTimestamp(): timestamp(0), color(0, 0, 0) {}
+  int32_t timestamp;
+  Color color;
+};
+
+class PacketsLog {
+public:
+  void ProcessRadioPacket(const MagicPathRadioPacket& p) {
+    if (p.id >= colors_.size()) {
+      LOG_WRN("Unexpected radio packet with ID = %d", p.id);
+      return;
+    }
+
+    if (p.configure_mode) {
+      background_color_ = Color(p.r_background, p.g_background, p.b_background);
+    }
+
+    colors_[p.id].timestamp = k_uptime_get();
+    colors_[p.id].color = Color(p.r, p.g, p.b);
+  }
+
+  Color GetColor() const {
+    const auto current_t = k_uptime_get();
+    uint16_t r = 0, g = 0, b = 0;
+    bool see_something = false;
+    for (const auto& entry: colors_) {
+      if (entry.timestamp != 0 && current_t - entry.timestamp < 3000) {
+        r += entry.color.r;
+        g += entry.color.g;
+        b += entry.color.b;
+        see_something = true;
+      }
+    }
+
+    if (!see_something) {
+      return background_color_;
+    }
+
+    return Color(std::min<uint16_t>(255, r), std::min<uint16_t>(255, g), std::min<uint16_t>(255, b));
+  }
+
+private:
+  Color background_color_ = {0, 0, 0};
+  std::array<ColorAndTimestamp, 20> colors_;
+};
 
 void main(void) {
   LOG_WRN("Hello! Application started successfully.");
-
 
   Cc1101 cc1101;
   cc1101.Init();
   cc1101.SetChannel(1);
 
   RgbLed led;
+  PacketsLog log;
 
-  Color colors[] = {{255, 0, 0}, {0, 255, 0}, {0, 0, 255}};
-  uint8_t currentColor = 0;
-  auto t1 = RunEvery([&](){
-    led.SetColorSmooth(colors[currentColor], 1000);
-    currentColor = (currentColor + 1) % 3;
-  }, 2000);
+  auto t1 = RunEvery([&led, &log](){
+    auto c = log.GetColor();
+    LOG_DBG("New color is %d %d %d", c.r, c.g, c.b);
+    led.SetColorSmooth(c, 1000);
+  }, 1000);
 
   while (true) {
-    UsbHostPacket pkt_2;
-    if (cc1101.Receive(100, &pkt_2)) {
-      LOG_INF("Got packet from %d", pkt_2.From);
+    MagicPathRadioPacket pkt;
+    for (int ch = 0; ch < 4; ++ch) {
+      cc1101.SetChannel(ch);
+      if (cc1101.Receive(63, &pkt)) {
+        LOG_DBG("Got packet! ID=%d, R=%d, G=%d, B=%d", pkt.id, pkt.r, pkt.g, pkt.b);
+        log.ProcessRadioPacket(pkt);
+      }
     }
 
-    UsbHostPacket pkt_3;
-    pkt_3.From = 17;
-    cc1101.Transmit(pkt_3);
-
-    k_sleep(100);
+    k_sleep(810);
   }
 }

851f32ca46fd371f05d3646d9db15840c7b3aad3
Alexey Eremin <a.eremin.msu@gmail.com>
2020-03-06 19:47:35 +0100

    Remove BT_GATT_ATTRIBUTE re-definition
    Not needed after https://github.com/zephyrproject-rtos/zephyr/pull/23167 was merged in

--- common/bluetooth.h
+++ common/bluetooth.h
@@ -3,19 +3,6 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/uuid.h>
 
-// Zephyr's own definition of BT_GATT_ATTRIBUTE is not compatible with C++.
-// https://github.com/zephyrproject-rtos/zephyr/pull/23167 send to fix it.
-#undef BT_GATT_ATTRIBUTE
-#define BT_GATT_ATTRIBUTE(_uuid, _perm, _read, _write, _value) \
-  {                                                            \
-    .uuid = _uuid,                                             \
-    .read = _read,                                             \
-    .write = _write,                                           \
-    .user_data = _value,                                       \
-    .handle = 0,                                               \
-    .perm = _perm,                                             \
-  }
-
 // Service UUID 8ec87060-8865-4eca-82e0-2ea8e45e8221
 // This is randomly-generated vendor-specific (i.e. Ostranna's) UUID.
 // It's expected to be present on all bluetooth Ostranna devices

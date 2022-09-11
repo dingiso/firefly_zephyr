62d26bcd4d51368d4fa63412813dc17366904eae
Alexey Eremin <a.eremin.msu@gmail.com>
2022-09-12 00:00:14 +0200

    Revert "Break the test to check that it will be caught by on-device test"
    This reverts commit 8602709419d8819be242e5623852dac5cae81841.

--- smoke_test/main.cpp
+++ smoke_test/main.cpp
@@ -81,7 +81,7 @@ TEST(TimerTest, RunsEvery) {
   k_sleep(K_MSEC(10));
   ASSERT_EQ(counter, 3);
   k_sleep(K_MSEC(10));
-  ASSERT_EQ(counter, 44);
+  ASSERT_EQ(counter, 4);
 }
 
 TEST(EepromTest, CanReadWritten) {

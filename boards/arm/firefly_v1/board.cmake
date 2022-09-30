97926d7e2c77abfddd590537feeb664721e9cbd3
Alexey Eremin <a.eremin.msu@gmail.com>
2022-09-30 18:37:42 +0200

    Remove all flashing runners except blackmagicprobe

--- boards/arm/firefly_v1/board.cmake
+++ boards/arm/firefly_v1/board.cmake
@@ -1,9 +1,2 @@
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
-
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
-board_runner_args(pyocd "--target=nrf52" "--frequency=4000000")
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 
--- boards/arm/locket/board.cmake
+++ boards/arm/locket/board.cmake
@@ -1,6 +1,2 @@
-# 'reset_config none separate' is required as there is no reset pin on the locket.
-board_runner_args(openocd "--cmd-pre-init=reset_config none separate")
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 

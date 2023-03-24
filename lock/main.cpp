fc1c1f8e0f1d18a8e095af64fb7b7cf414cc34c6
Alexey Eremin <a.eremin.msu@gmail.com>
2023-03-24 23:52:57 +0100

    Configure and test PWM devices (leds, buzzer)

--- lock/main.cpp
+++ lock/main.cpp
@@ -3,35 +3,39 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/random/rand32.h>
 
-#include "keyboard.h"
+#include "buzzer.h"
 #include "eeprom.h"
+#include "keyboard.h"
+#include "rgb_led.h"
+#include "sequences.h"
 
 LOG_MODULE_REGISTER();
-
-constexpr gpio_dt_spec led_en = GPIO_DT_SPEC_GET(DT_NODELABEL(led_en), gpios);
-constexpr gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_NODELABEL(led_b), gpios);
 
 constexpr gpio_dt_spec reed_switch = GPIO_DT_SPEC_GET(DT_NODELABEL(reed_switch), gpios);
 constexpr gpio_dt_spec sw1 = GPIO_DT_SPEC_GET(DT_NODELABEL(button_sw1), gpios);
 constexpr gpio_dt_spec sw2 = GPIO_DT_SPEC_GET(DT_NODELABEL(button_sw2), gpios);
+
+RgbLed led;
+RgbLedSequencer led_sequencer(led);
+Buzzer buzzer;
 
 using namespace std;
 
 int main() {
   LOG_WRN("Hello! Application started successfully.");
 
-  gpio_pin_configure_dt(&led_en, GPIO_OUTPUT);
-  gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-
   for (auto& spec : {reed_switch, sw1, sw2}) {
     gpio_pin_configure_dt(&spec, GPIO_INPUT);
   }
 
-  gpio_pin_set_dt(&led_en, 0);
-
-  Keyboard keyboard([](char c) {
+  Keyboard keyboard([&](char c) {
       LOG_INF("Pressed %c", c);
+      if (c == '*') {
+        buzzer.Beep(200, 700, 300);
+      }
   });
+
+  led_sequencer.StartOrRestart(lsqStart);
 
   eeprom::EnablePower();
 
@@ -47,6 +51,7 @@ int main() {
   }
 
   LOG_ERR("EEPROM read/write test finished");
+  buzzer.Beep(100, 600, 100);
 
   initializer_list<pair<gpio_dt_spec, const char*>> buttons{{reed_switch, "Reed"}, {sw1, "Sw1"}, {sw2, "Sw2"}};
 
@@ -56,10 +61,7 @@ int main() {
         LOG_INF("%s pressed", name);
       }
     }
-    gpio_pin_set_dt(&led, 0);
-    k_sleep(K_MSEC(250));
-    gpio_pin_set_dt(&led, 1);
-    k_sleep(K_MSEC(250));
+    k_sleep(K_MSEC(100));
   }
 }
 

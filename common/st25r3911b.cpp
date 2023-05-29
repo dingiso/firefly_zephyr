19ba1cb421c913d0b78179477770e0163f645d7d
Alexey Eremin <a.eremin.msu@gmail.com>
2023-05-29 12:43:42 +0200

    Reformat file

--- common/st25r3911b.cpp
+++ common/st25r3911b.cpp
@@ -24,14 +24,14 @@ void St25r3911b::Init() {
   EnableOscillator();
 
   WriteRegister(ModeDefinitionRegister{
-    .nfc_ar = false,
-    .om = ModeDefinitionRegister::OperationMode::Iso14443A,
-    .targ = false,
+      .nfc_ar = false,
+      .om = ModeDefinitionRegister::OperationMode::Iso14443A,
+      .targ = false,
   });
 
   WriteRegister(BitRateDefinitionRegister{
-    .rx_rate = BitRate::Bpsk_106,
-    .tx_rate = BitRate::Bpsk_106,
+      .rx_rate = BitRate::Bpsk_106,
+      .tx_rate = BitRate::Bpsk_106,
   });
 
   SendCommand(DirectCommand::AnalogPreset);
@@ -51,52 +51,52 @@ void St25r3911b::Init() {
 void St25r3911b::InitIrq() {
   k_sem_init(&irq_sem_, 0, 1);
 
-	PW_ASSERT(device_is_ready(irq_pin_spec_->port));
-	PW_ASSERT(gpio_pin_configure_dt(irq_pin_spec_, GPIO_INPUT) == 0);
-	gpio_init_callback(&gpio_cb_, St25r3911b::irq_pin_cb, BIT(irq_pin_spec_->pin));
-	PW_ASSERT(gpio_add_callback(irq_pin_spec_->port, &gpio_cb_) == 0);
-	PW_ASSERT(gpio_pin_interrupt_configure_dt(irq_pin_spec_, GPIO_INT_EDGE_TO_ACTIVE) == 0);
+  PW_ASSERT(device_is_ready(irq_pin_spec_->port));
+  PW_ASSERT(gpio_pin_configure_dt(irq_pin_spec_, GPIO_INPUT) == 0);
+  gpio_init_callback(&gpio_cb_, St25r3911b::irq_pin_cb, BIT(irq_pin_spec_->pin));
+  PW_ASSERT(gpio_add_callback(irq_pin_spec_->port, &gpio_cb_) == 0);
+  PW_ASSERT(gpio_pin_interrupt_configure_dt(irq_pin_spec_, GPIO_INT_EDGE_TO_ACTIVE) == 0);
 }
 
 void St25r3911b::DisableInterrupts() {
   WriteRegister(MainInterruptConfigRegister({
-    .mask_col = true,
-    .mask_txe = true,
-    .mask_rxe = true,
-    .mask_rxs = true,
-    .mask_wl = true,
-    .mask_osc = true,
+      .mask_col = true,
+      .mask_txe = true,
+      .mask_rxe = true,
+      .mask_rxs = true,
+      .mask_wl = true,
+      .mask_osc = true,
   }));
 
   WriteRegister(MaskTimerAndNfcInterruptRegister({
-    .mask_nfct = true,
-    .mask_cat = true,
-    .mask_cac = true,
-    .mask_eof = true,
-    .mask_eon = true,
-    .mask_gpe = true,
-    .mask_nre = true,
-    .mask_dct = true,
+      .mask_nfct = true,
+      .mask_cat = true,
+      .mask_cac = true,
+      .mask_eof = true,
+      .mask_eon = true,
+      .mask_gpe = true,
+      .mask_nre = true,
+      .mask_dct = true,
   }));
 
   WriteRegister(MaskErrorAndWakeUpInterruptRegister({
-    .mask_wcap = true,
-    .mask_wph = true,
-    .mask_wam = true,
-    .mask_wt = true,
-    .mask_err1 = true,
-    .mask_err2 = true,
-    .mask_par = true,
-    .mask_crc = true,
+      .mask_wcap = true,
+      .mask_wph = true,
+      .mask_wam = true,
+      .mask_wt = true,
+      .mask_err1 = true,
+      .mask_err2 = true,
+      .mask_par = true,
+      .mask_crc = true,
   }));
 }
 
 St25r3911b::InterruptRegisters St25r3911b::GetInterrupts() {
   // Per datasheet, the interrupt registers are reset after being read.
   return {
-    ReadRegister<MainInterruptRegister>(),
-    ReadRegister<TimerAndNfcInterruptRegister>(),
-    ReadRegister<ErrorAndWakeUpInterruptRegister>(),
+      ReadRegister<MainInterruptRegister>(),
+      ReadRegister<TimerAndNfcInterruptRegister>(),
+      ReadRegister<ErrorAndWakeUpInterruptRegister>(),
   };
 }
 

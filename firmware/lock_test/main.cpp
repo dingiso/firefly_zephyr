490458ca8eddd8d988fae7f2a143ce820ca6c505
Alexey Eremin <a.eremin.msu@gmail.com>
2024-08-20 14:01:08 +0200

    Revert "Disable buzzer for now"
    This reverts commit 18dd633320dc4b25a6a81094dac3cc944a24d6a7.

--- firmware/lock_test/main.cpp
+++ firmware/lock_test/main.cpp
@@ -215,9 +215,10 @@ int main() {
 
   int num_failures = RUN_ALL_TESTS();
   if (!num_failures) {
+    buzzer.Beep(100, 600, 100);
     printk("All tests passed!\n");
   } else {
-    printk("%d tests failed!\n", num_failures);
+    buzzer.Beep(100, 600, 1000);
   }
 
   pw::system::GetRpcServer().RegisterService(echo_service);

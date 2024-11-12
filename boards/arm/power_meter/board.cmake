2118025c18320c49642229f1e1848a5d8b74c8b0
Alexey Eremin <a.eremin.msu@gmail.com>
2024-11-12 22:49:31 +0100

    Use blackmagicprobe by default for the power_meter board

--- boards/arm/power_meter/board.cmake
+++ boards/arm/power_meter/board.cmake
@@ -1,3 +1,3 @@
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 
